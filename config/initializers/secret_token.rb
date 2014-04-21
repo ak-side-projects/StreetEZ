5e805a503bf34b04bc00fcb6209d807f74056c81
Chas <student@appacademy.io>
2014-04-21 11:57:36 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StreetEZ::Application.config.secret_key_base = '8a2948dd8622bdfa8c984c2a7b718a1cbcbf56ed1adad9b93b8018c65a110221a4fe6290ebb5b856ecc959514d85db9ce54a101aa3333d47331ab3a7fa0a69d2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StreetEZ::Application.config.session_store :cookie_store, key: '_StreetEZ_session'
