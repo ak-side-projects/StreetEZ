a8925cd2f7a9ba3e6bff33c3fcb0790cc3211b9e
Andrew Kayvanfar <akayvanfar@gmail.com>
2014-04-22 14:46:09 -0400

    sets up aws, paperclip, S3

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(:default, Rails.env)
 module StreetEZ
   class Application < Rails::Application
     config.assets.initialize_on_precompile = false
-    
+
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: true,
@@ -20,6 +20,15 @@ module StreetEZ
         request_specs: true
       g.fixture_replacement :factory_girl, :dir => "spec/factories"
     end
+
+    config.paperclip_defaults = {
+      :storage => :s3,
+      :s3_credentials => {
+        :bucket => ENV['S3_BUCKET'], #these values safely stored in application.yml thanks to figaro!
+        :access_key_id => ENV['S3_ACCESS_KEY'],
+        :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
+      }
+    }
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
