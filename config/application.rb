4a8fd620d114172c40beeb4c0dd1ec4360716588
Andrew Kayvanfar <akayvanfar@gmail.com>
2014-04-21 23:04:09 -0400

    fixes heroku

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,8 @@ Bundler.require(:default, Rails.env)
 
 module StreetEZ
   class Application < Rails::Application
-
+    config.assets.initialize_on_precompile = false
+    
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: true,
